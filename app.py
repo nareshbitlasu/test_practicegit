e855f039ff84f13ae95e1083767f165e6cb359ca
naresh bitla <nareshbitla@gmail.com>
2026-09-09 07:12:07 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# Declare variables
+num1 = 1852
+num2 = 1524
+
+# Basic arithmetic operations
+sum_result = num1 + num2
+product = num1 * num2
+division = num1 / num2
+remainder = num1 % num2  # Modulus operator
+
+print(f"Sum: {sum_result}, Product: {product}")
+print(f"Division: {division}, Remainder: {remainder}")
